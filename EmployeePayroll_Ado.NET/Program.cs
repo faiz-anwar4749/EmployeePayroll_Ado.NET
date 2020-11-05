d967d8e11ab42514c0a9078f9f54c446d3740e65
Faiz <faiz.anwar4749@gmail.com>
2020-11-05 08:18:27 +0530

    [Faiz] Add: Welcome File

---
+++ EmployeePayroll_Ado.NET/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EmployeePayroll_Ado.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Program!");
+        }
+    }
+}
